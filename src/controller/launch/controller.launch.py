42c4513ff7159d67209a6238c5a833424cdc365b
Marcus Kornmann <marcus.kornmann@icloud.com>
2026-02-03 12:08:49 +0100

    parametrize node

--- src/controller/launch/controller.launch.py
+++ src/controller/launch/controller.launch.py
@@ -1,13 +1,23 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import PathJoinSubstitution
+
 
 
 def generate_launch_description():
+    parameters_file = PathJoinSubstitution([
+        FindPackageShare('controller'),
+        'resource',
+        'parameters.yaml',
+    ])
+
     controller_node = Node(
         package='controller',
         executable='controller_node',
         name='controller_node',
         output='screen',
+        parameters=[parameters_file],
     )
 
     return LaunchDescription([
